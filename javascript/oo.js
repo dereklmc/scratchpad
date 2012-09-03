841fb427c4f93510b76625cf693279927c2f8190
Derek McLean <derek.l.mclean@gmail.com>
2012-09-03 12:16:06 -0700

    Update javascript/oo.js
    Found code on desktop.

--- javascript/oo.js
+++ javascript/oo.js
@@ -1,1 +1,21 @@
-...
+function Class(Ancestor, attributes) {
+  var init;
+	if (attributes.hasOwnProperty('init')) {
+		init = attributes.init;
+		delete attributes.init;
+	} else {
+		init = function () {};
+	}
+	function NewClass() {
+		var attr;
+		for (attr in attributes) {
+			if (attributes.hasOwnProperty(attr)) {
+				this[attr] = attributes[attr];
+			}
+		}
+		this.init = init;.apply(this, arguments);
+	}
+	NewClass.prototype = Ancestor;
+	NewClass.prototype.init = init;
+	return NewClass;
+}
